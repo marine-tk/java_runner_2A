868a19d16cef7e7f9abc59ede6d3024bff369852
Marine <marine.tsen-kung@ensea.fr>
2021-11-22 11:42:08 +0100

    runner

---
+++ src/Lifebar.java
@@ -0,0 +1,10 @@
+public class Lifebar extends AnimatedThing{
+
+    // Constructeur
+    public Lifebar(double x, double y, int attitude, int index, int maxIndex, double l, double h, String fileName) {
+        super(x, y, attitude, index, maxIndex, l, h, fileName);
+    }
+
+
+
+}
